9a8f8cf6c6ae51367ce9c5e81a269c26ce024b94
Paul Kiernan <paulk@rwebs.co.uk>
2016-06-11 11:48:06 +0100

    adding tests project

---
+++ Handsey.Practices.ViewModelBuilding.Test/ContentHandlerPipelineTests.cs
@@ -0,0 +1,82 @@
+﻿namespace Handsey.Practices.ViewModelBuilding.Test
+{
+    using Moq;
+    using NUnit.Framework;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    [TestFixture]
+    public class ContentHandlerPipelineTests
+    {
+        #region Fields
+
+        private Mock<IHandlerResolver> _handlerResolver;
+        private IContentHandlerPipeline _contentHandlerPipeline;
+
+        #endregion Fields
+
+        #region Tests
+
+        [SetUp]
+        public void Setup()
+        {
+            _handlerResolver = new Mock<IHandlerResolver>();
+            _contentHandlerPipeline = new ContentHandlerPipeline(_handlerResolver.Object);
+        }
+
+        [Test]
+        public void Raise_THandlerArgs_NoHanldersMatched()
+        {
+            // Arrange
+            _handlerResolver
+               .Setup(x => x.ResolverAll<IHandler<HandlerArgs<EPiServerModel, ViewModel>>>())
+               .Returns(Enumerable.Empty<IHandler<HandlerArgs<EPiServerModel, ViewModel>>>());
+
+            // Act
+            _contentHandlerPipeline.Raise(new HandlerArgs<EPiServerModel, ViewModel>(new EPiServerModel(), new ViewModel()));
+
+            // Assert
+            _handlerResolver.Verify(x => x.ResolverAll<IHandler<HandlerArgs<EPiServerModel, ViewModel>>>(), Times.Once, "Handler resolver was one called");
+        }
+
+        [Test]
+        public void Raise_THandlerArgs_HandlersResolvedAndInvoked()
+        {
+            Mock<IHandler<HandlerArgs<EPiServerModel, ViewModel>>> handler1 = new Mock<IHandler<HandlerArgs<EPiServerModel, ViewModel>>>();
+            Mock<IHandler<HandlerArgs<EPiServerModel, ViewModel>>> handler2 = new Mock<IHandler<HandlerArgs<EPiServerModel, ViewModel>>>();
+
+            // Arrange
+            _handlerResolver
+                .Setup(x => x.ResolverAll<IHandler<HandlerArgs<EPiServerModel, ViewModel>>>())
+                .Returns(new List<IHandler<HandlerArgs<EPiServerModel, ViewModel>>>()
+                             {
+                                 handler1.Object,
+                                 handler2.Object
+                             });
+
+            // Act
+            _contentHandlerPipeline.Raise(new HandlerArgs<EPiServerModel, ViewModel>(new EPiServerModel(), new ViewModel()));
+
+            // Assert
+            _handlerResolver.Verify(x => x.ResolverAll<IHandler<HandlerArgs<EPiServerModel, ViewModel>>>(), Times.Once, "Handler resolver was one called");
+            handler1.Verify(x => x.Handle(It.IsAny<HandlerArgs<EPiServerModel, ViewModel>>()), Times.Once());
+            handler2.Verify(x => x.Handle(It.IsAny<HandlerArgs<EPiServerModel, ViewModel>>()), Times.Once());
+        }
+
+        #endregion Tests
+
+        #region Data
+
+        public class EPiServerModel
+        {
+            public string Title { get; set; }
+        }
+
+        public class ViewModel
+        {
+            public string Title { get; set; }
+        }
+
+        #endregion Data
+    }
+}
---
+++ Handsey.Practices.ViewModelBuilding.Test/ViewModelBuilderTests.cs
@@ -0,0 +1,99 @@
+﻿namespace Handsey.Practices.ViewModelBuilding.Test
+{
+    using Moq;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ViewModelBuilderTests
+    {
+        #region Fields
+
+        private Mock<IContentHandlerPipeline> _contentHandlerPipeline;
+        private Mock<IPropertyMapper> _propertyMapper;
+        private IViewModelBuilder _viewModelBuilder;
+
+        #endregion Fields
+
+        #region Tests
+
+        [SetUp]
+        public void Setup()
+        {
+            _contentHandlerPipeline = new Mock<IContentHandlerPipeline>();
+            _propertyMapper = new Mock<IPropertyMapper>();
+            _viewModelBuilder = new ViewModelBuilder(_propertyMapper.Object, _contentHandlerPipeline.Object);
+        }
+
+        [Test]
+        public void Build_EpiServerModel_ModelMappedAndBuilt()
+        {
+            // Arrange
+            _propertyMapper.Setup(
+                x => x.Map<EPiServerModel, ViewModel>(It.IsAny<EPiServerModel>(), It.IsAny<ViewModel>()));
+
+            // Act
+            ViewModel viewModel = _viewModelBuilder.Build<EPiServerModel, ViewModel>(new EPiServerModel() { Title = "MoFo" });
+
+            // Assert
+            Assert.That(viewModel, Is.Not.Null);
+            _contentHandlerPipeline.Verify(c => c.Raise(It.IsAny<HandlerArgs<EPiServerModel, ViewModel>>()), Times.Once());
+            _propertyMapper.Verify(x => x.Map(It.IsAny<EPiServerModel>(), It.IsAny<ViewModel>()), Times.AtLeastOnce());
+        }
+
+        [Test]
+        public void Build_EpiServerModel_ModelMappingFalseSoBuiltAndNotMapped()
+        {
+            // Arrange
+            _propertyMapper.Setup(
+                x => x.Map<EPiServerModel, ViewModel>(It.IsAny<EPiServerModel>(), It.IsAny<ViewModel>()));
+
+            // Act
+            ViewModel viewModel = _viewModelBuilder.Build<EPiServerModel, ViewModel>(new EPiServerModel() { Title = "MoFo" }, false);
+
+            // Assert
+            Assert.That(viewModel, Is.Not.Null);
+            _contentHandlerPipeline.Verify(c => c.Raise(It.IsAny<HandlerArgs<EPiServerModel, ViewModel>>()), Times.Once());
+            _propertyMapper.Verify(x => x.Map(It.IsAny<EPiServerModel>(), It.IsAny<ViewModel>()), Times.Never);
+        }
+
+        [Test]
+        public void Build_EpiServerModelAndFormModel_ModelMappingTrueSoBuiltAndMapped()
+        {
+            // Arrange
+            _propertyMapper.Setup(
+                x => x.Map<EPiServerModel, ViewModel>(It.IsAny<EPiServerModel>(), It.IsAny<ViewModel>()));
+
+            // Act
+            ViewModel viewModel = _viewModelBuilder.Build<FormModel, EPiServerModel, ViewModel>(new FormModel() { Step = 1 }, new EPiServerModel() { Title = "MoFo" });
+
+            // Assert
+            Assert.That(viewModel, Is.Not.Null);
+            _contentHandlerPipeline.Verify(c => c.Raise(It.IsAny<HandlerArgs<EPiServerModel, ViewModel>>()), Times.Once());
+            _propertyMapper.Verify(x => x.Map(It.IsAny<EPiServerModel>(), It.IsAny<ViewModel>()), Times.AtLeastOnce());
+        }
+
+        #endregion Tests
+
+
+
+        #region Data
+
+        private class EPiServerModel
+        {
+            public string Title { get; set; }
+        }
+
+        private class FormModel
+        {
+            public int Step { get; set; }
+        }
+
+        private class ViewModel
+        {
+            public string Title { get; set; }
+        }
+
+        #endregion Data
+
+    }
+}
